a712818ea372aad31cd680e2a729125636118ac0
PsionicCat Balflear <psioniccat.programming@outlook.com>
2015-05-14 09:42:20 +0800

    * routers added for socket.io activities binding

---
+++ core/server/routes/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Íò²© on 2015/5/14.
+ */
+var user    = require('./user');
+
+module.exports = {
+    user: user
+};
---
+++ core/server/routes/user.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Íò²© on 2015/5/14.
+ */
+var ss         = require('socket.io-stream'),
+    user       = require('../controllers/user'),
+    userRoutes;
+
+userRoutes = function (socket) {
+
+    socket.on('check nickname', user.checkNickname(socket));
+
+    socket.on('initialize user', user.initializeUser(socket));
+
+    socket.on('disconnect', user.signOff(socket));
+
+    socket.on('create room', user.createRoom(socket));
+
+    socket.on('join room', user.joinRoom(socket));
+
+    socket.on('leave room', user.leaveRoom(socket));
+
+    socket.on('send text message', user.sendTextMessage(socket));
+
+    ss(socket).on('share file', user.shareFile(socket));
+
+    socket.on('send audio message', user.sendAudioMessage(socket));
+
+};
+
+module.exports = userRoutes;
