26cd6819408fcaf03789472ac8b90a19d2f52278
PsionicCat Balflear (Wanbo Lu) <psioniccat.programming@outlook.com>
2015-06-03 11:38:50 +0800

    * config.example.js updated   * icollege connection support   * dnode connection support   * whisper-hub connection support     * whisper hub is not build yet, we are working on it

--- config.example.js
+++ config.example.js
@@ -1,41 +1,80 @@
 /**
- * Created by Íò²© on 2015/4/22.
+ * # config example file
  */
-
 var path = require('path'),
     config;
 
 config = {
     // ### Production
-    // When running Ghost in the wild, use the production environment
-    // Configure your URL and mail settings here
-    production: {
-        url: 'http://whispers.com',
+    // When running whisper in the wild, it will be in **production** environment
 
+    // Configure your URL here
+    production: {
+        // the example url doesn't exists at all. it's just an example.
+        url: 'http://whisper.com',
+
+        // current whisper server host and port
+        // host and port will be passed to node's `net.Server#listen()`
         server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            host: '0.0.0.0',
+            // for iisnode set this to `process.env.PORT`
             port: '1333'
+        },
+        dnode: {
+            port: '1332'
+        },
+        // **UNFINISHED** whisper hub is a hub for multi-whispers distributed system
+        //     in order to transferring messages through different whisper instances
+        // so far we haven't get this done yet.
+        hub: {
+            host: '127.0.0.1',
+            port: '1444'
+        },
+        // icollege is used for authorization and other supports (through RESTful APIs)
+        icollege: {
+            host: '127.0.0.1',
+            port: '1222'
+        },
+        // beanstalkd is used for database queuing
+        beanstalkd: {
+            host: '127.0.0.1',
+            port: '1555'
         }
     },
 
-    // ### Development **(default)**
+    // ### Development
+    // When running whisper for system developing test (like black box testing),
+    // whisper is running in **development** mode,
+    // which means that the server is running only for localhost
     development: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://localhost:1333',
+        url: 'http://127.0.0.1:1333',
 
-        // server config
+        // current whisper server host and port
+        // host and port will be passed to node's `net.Server#listen()`
         server: {
-            // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            // for iisnode set this to `process.env.PORT`
             port: '1333'
         },
-        // paths config
-        paths: {
-            // do we need this?
-            contentPath: path.join(__dirname, '/content/')
+        dnode: {
+            port: '1332'
+        },
+        // **UNFINISHED** whisper hub is a hub for multi-whispers distributed system
+        //     in order to transferring messages through different whisper instances
+        // so far we haven't get this done yet.
+        hub: {
+            host: '127.0.0.1',
+            port: '1444'
+        },
+        // icollege is used for authorization and other supports (through RESTful APIs)
+        icollege: {
+            host: '127.0.0.1',
+            port: '1222'
+        },
+        // beanstalkd is used for database queuing
+        beanstalkd: {
+            host: '127.0.0.1',
+            port: '1555'
         },
 
         // logging level
@@ -44,15 +83,39 @@ config = {
     },
 
     // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
+    // Used when developing whisper to run tests and check the health of whisper
+    // Uses a **different** port number
     testing: {
-        url: 'http://127.0.0.1:1333',
+        url: 'http://127.0.0.1:1332',
 
+        // current whisper server host and port
+        // host and port will be passed to node's `net.Server#listen()`
         server: {
             host: '127.0.0.1',
-            port: '1333'
+            // for iisnode set this to `process.env.PORT`
+            port: '1323'
         },
+        dnode: {
+            port: '1322'
+        },
+        // **UNFINISHED** whisper hub is a hub for multi-whispers distributed system
+        //     in order to transferring messages through different whisper instances
+        // so far we haven't get this done yet.
+        hub: {
+            host: '127.0.0.1',
+            port: '1444'
+        },
+        // icollege is used for authorization and other supports (through RESTful APIs)
+        icollege: {
+            host: '127.0.0.1',
+            port: '1222'
+        },
+        // beanstalkd is used for database queuing
+        beanstalkd: {
+            host: '127.0.0.1',
+            port: '1555'
+        },
+
         logging: false
     }
 };
