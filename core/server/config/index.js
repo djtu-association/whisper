bf832abcb3c60a6580598f0c1aadd1fb5bbb9dfc
PsionicCat Balflear (Wanbo Lu) <psioniccat.programming@outlook.com>
2015-06-03 11:34:06 +0800

    * config module updated   * all console.log() are replaced with logger module   * useless function deprecated

--- core/server/config/index.js
+++ core/server/config/index.js
@@ -1,9 +1,9 @@
 /**
- * Created by PsionicCat Balflear on 2015/5/3.
+ * @author Psioniccat Balflear (Wanbo Lu)
  */
 
-var path           = require('path'),
-    Promise        = require('bluebird'),
+var path          = require('path'),
+    Promise       = require('bluebird'),
     fs              = require('fs'),
     validator      = require('validator'),
     url             = require('url'),
@@ -11,13 +11,13 @@ var path           = require('path'),
 
     packageInfo   = require('../../../package.json'),
 
-    appRoot        = path.resolve(__dirname, '../../..'),
-    corePath       = path.resolve(appRoot, 'core/'),
+    logger          = require('../logger'),
+    appRoot       = path.resolve(__dirname, '../../..'),
+    corePath      = path.resolve(appRoot, 'core/'),
     testingEnvs   = ['testing'],
     defaultConfig = {};
 
 function ConfigManager(config) {
-    console.log('ConfigManager constructor from \\core\\server\\config\\index.js');
     this._config = {};
 
     //this.urlFor = configUrl.urlFor;
@@ -28,7 +28,6 @@ function ConfigManager(config) {
 }
 
 ConfigManager.prototype.getSocket = function () {
-    console.log('config getSocket begining');
     var socketConfig,
         values = {
             path: path.join(this._config.paths.contentPath, process.env.NODE_ENV + '.socket'),
@@ -56,17 +55,13 @@ ConfigManager.prototype.getSocket = function () {
 };
 
 ConfigManager.prototype.getSocket = function () {
-    console.log('config get socket');
     var socketConfig,
         values = {
             path: path.join(this._config.paths.contentPath, process.env.NODE_ENV + '.socket'),
             permissions: '660'
         };
 
-    console.log(values);
-    console.log(this._config.server.hasOwnProperty('socket'));
     if (this._config.server.hasOwnProperty('socket')) {
-        console.log(this._config);
         socketConfig = this._config.server.socket;
 
         if (_.isString(socketConfig)) {
@@ -87,8 +82,6 @@ ConfigManager.prototype.getSocket = function () {
 };
 
 ConfigManager.prototype.init = function (rawConfig) {
-    console.log('ConfigManager init from \\core\\server\\config\\index.js');
-    // 由于this的灵活性，我们需要保留当前对象的“句柄”来进行操作
     var self = this;
 
     self.set(rawConfig);
@@ -101,8 +94,6 @@ ConfigManager.prototype.set = function (config) {
         contentPath,
         subdir;
 
-
-    // 使用merge函数避免undefined带来的问题
     _.merge(this._config, config);
 
     this._config.paths = this._config.paths || {};
@@ -116,16 +107,15 @@ ConfigManager.prototype.set = function (config) {
 
     subdir = localPath === '/' ? '' : localPath;
 
+
     contentPath = this._config.paths.contentPath || path.resolve(appRoot, 'content');
 
-    // 通常的配置项都应在config.js中
-    // 但存在一些动态配置项或特殊原因
-    // 无法或不应写入静态文件的配置项
-    // 在此一并通过merge合并到配置中
+    // merge dynamic parameters into config object
     _.merge(this._config, {
         whispersVersion: packageInfo.version,
         paths: {
             appRoot : appRoot,
+            contentPath:      contentPath,
             subdir: subdir,
             config: this._config.paths.config || path.join(appRoot, 'config.js'),
             configExample: path.join(appRoot, 'config.example.js'),
@@ -133,9 +123,6 @@ ConfigManager.prototype.set = function (config) {
         }
     });
 
-    //configUrl.setConfig(this._config);
-
-    // 将处理好的配置复制到当前config对象
     _.extend(this, this._config);
 };
 
@@ -144,7 +131,6 @@ ConfigManager.prototype.get = function () {
 };
 
 ConfigManager.prototype.load = function (configFilePath) {
-    console.log('ConfigManager load from \\core\\server\\config\\index.js');
     var self = this;
 
     self._config.paths.config = process.env.WHISPERS_CONFIG || configFilePath || self._config.paths.config;
@@ -167,34 +153,29 @@ ConfigManager.prototype.load = function (configFilePath) {
 };
 
 ConfigManager.prototype.writeFile = function () {
-    console.log('ConfigManager writefile from \\core\\server\\config\\index.js')
     var configPath = this._config.paths.config,
         configExamplePath = this._config.paths.configExample;
 
-    return new Promise(function (resolve, reject) {
+    return new Promise(function (resolve) {
         fs.exists(configExamplePath, function checkTemplate(templateExists) {
             var readStream,
-                writeStream,
-                error;
+                writeStream;
 
             if (!templateExists) {
-                error = new Error('无法定位配置文件。');
-                error.context = appRoot;
-                error.help = '请检查部署实例的config.js或者config.example.js文件是否可用。';
-
-                return reject(error);
+                var message = '\nUnable to read or write configure file',
+                    detail = '\nLocation: ' + appRoot,
+                    hint = '\nPlease check config.js and config.example.js';
+                logger.throwAndLog(message, detail, hint);
             }
 
             readStream = fs.createReadStream(configExamplePath);
             readStream.on('error', function (error) {
-                // todo: log this error
-                reject(error);
+                return logger.reject(error);
             });
 
             writeStream = fs.createWriteStream(configPath);
             writeStream.on('error', function (error) {
-                //todo: log this error
-                reject(error);
+                return logger.reject(error);
             });
 
             writeStream.on('finish', resolve);
@@ -205,63 +186,62 @@ ConfigManager.prototype.writeFile = function () {
 };
 
 ConfigManager.prototype.readFile = function (envVal) {
-    console.log('ConfigManager readfile envVal:' + envVal);
     return require(this._config.paths.config)[envVal];
 };
 
 ConfigManager.prototype.validate = function () {
-    console.log('ConfigManager validate from \\core\\server\\config\\index.js');
     var envVal = process.env.Node_ENV,
         hasHostAndPort,
         config,
         parsedUrl;
 
-    console.log('ConfigManager validate env:' + envVal);
     try {
         config = this.readFile(envVal);
     } catch (error) {
-        return Promise.reject(error);
+        return logger.reject(error);
     }
 
+    var message;
     if (!config) {
-        //todo: log this error: cannot find the configuration for the current NODE_ENV
-        return Promise.reject(new Error('无法加载NODE_ENV为' + envVal + '时的配置。'));
+        message = '\n Unable to load configuration for Node_ENV==' + envVal;
+        return logger.reject(message);
     }
 
     if (!validator.isURL(config.url, {protocols: ['http', 'https']})) {
-        //todo: log this error: invalid site url
-        return Promise.reject(new Error('配置文件中指定的站点URL无效。'))
+        message = '\nInvalid site url in configuration file';
+        return logger.reject(message);
     }
 
     parsedUrl = url.parse(config.url || 'invalid', false, true);
 
-    if (/\/icollege(\/|$)/.test(parsedUrl.pathname)) {
-        //todo: log this error: path contains 'whispers'
-        return Promise.reject(new Error('部署实例所在路径不应包含whispers目录'));
+    if (/\/whisper(\/|$)/.test(parsedUrl.pathname)) {
+        message = '\nInstance path with \'whisper\' which is not allowed';
+        return logger.reject(message);
     }
 
     //todo: check icollege host and port in config.js
     hasHostAndPort = config.server && !!config.server.host && !!config.server.port;
 
     if (!config.server || !(hasHostAndPort)) {
-        //todo: log this error: invalid server configuration
-        return Promise.reject(new Error('无效的服务器主机或端口设定'));
+        message = '\nInvalid server host or port. ';
+        return logger.reject(message);
     }
 
     return Promise.resolve(config);
 };
 
-ConfigManager.prototype.isPrivacyDisabled = function (privacyFlag) {
-    if (!this.privacy) {
-        return false;
-    }
-
-    if (this.privacy.useTinfoil === true) {
-        return true;
-    }
-
-    return this.privacy[privacyFlag] === false;
-};
+// privacy settings is handled by icollege, so this function is deprecated
+//ConfigManager.prototype.isPrivacyDisabled = function (privacyFlag) {
+//    if (!this.privacy) {
+//        return false;
+//    }
+//
+//    if (this.privacy.useTinfoil === true) {
+//        return true;
+//    }
+//
+//    return this.privacy[privacyFlag] === false;
+//};
 
 /**
  * Check if any of the currently set config items are deprecated, and issues a warning.
@@ -276,9 +256,9 @@ ConfigManager.prototype.checkDeprecated = function () {
 ConfigManager.prototype.displayDeprecated = function (item, properties, address) {
     var self = this,
         property = properties.shift(),
-        errorText,
-        explanationText,
-        helpText;
+        message,
+        detail,
+        hint;
 
     address.push(property);
 
@@ -286,11 +266,16 @@ ConfigManager.prototype.displayDeprecated = function (item, properties, address)
         if (properties.length) {
             return self.displayDeprecated(item[property], properties, address);
         }
-        errorText = 'The configuration property [' + address.join('.').bold + '] has been deprecated.';
-        explanationText =  'This will be removed in a future version, please update your config.js file.';
-        helpText = 'Please check https://42.96.195.83/guanggu/icollege/blob/master/config.example.js for the most up-to-date example.';
-        errors.logWarn(errorText, explanationText, helpText);
+        message = 'The configuration property [' + address.join('.').bold + '] has been deprecated.';
+        detail = 'This will be removed in a future version, please update your config.js file.';
+        hint = 'Please check https://42.96.195.83/association/whisper/blob/master/config.example.js ' +
+            'for the most up-to-date example.';
+        logger.warn(message, detail, hint);
     }
 };
+
+if (testingEnvs.indexOf(process.env.NODE_ENV) > -1) {
+    defaultConfig  = require('../../../config.example')[process.env.NODE_ENV];
+}
 
 module.exports = new ConfigManager(defaultConfig);
